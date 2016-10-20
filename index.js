81d9256e3c9b9e05aede8db1058b1d67a1d5c265
Cameron Behar <0x24a537r9@gmail.com>
2016-10-19 20:32:42 -0700

    Initial commit of functional page.

---
+++ index.js
@@ -0,0 +1,31 @@
+var fields = ['skin', 'location', 'ink', 'layering', 'scarring', 'colors'];
+var form;
+
+$(function() {
+  form = document.forms[0];
+
+  $('input').change(onChange);
+});
+
+
+function onChange(e) {
+  var scale = {};
+  var field_count = 0;
+  for (var i = 0; i < fields.length; ++i) {
+    var field = fields[i];
+    if (form[field].value) {
+      scale[field] = form[field].value;
+      ++field_count;
+    }
+  }
+  
+  var total_points = 0;
+  for (var field in scale) {
+    $('.' + field + '-points').text(scale[field]);
+    total_points += parseInt(scale[field]);
+  }
+
+  if (field_count == fields.length) {
+    $('.total-points').text(total_points);
+  }
+}
